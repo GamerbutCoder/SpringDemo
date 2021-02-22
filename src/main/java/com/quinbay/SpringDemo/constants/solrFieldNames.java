bc29e650f420b3f68234bac8bd09f3ca403eb4d2
nagadeepakbhushan <nagadeepak000@gmail.com>
2021-02-22 18:24:51 +0530

    OpenFeign Integrated

---
+++ src/main/java/com/quinbay/SpringDemo/constants/solrFieldNames.java
@@ -0,0 +1,8 @@
+package com.quinbay.SpringDemo.constants;
+
+public interface solrFieldNames {
+    String NAME = "name";
+    String IN_STOCK = "isInStock";
+    String DESCRIPTION = "description";
+    String OFFER_PRICE = "offerPrice";
+}
--- src/main/java/com/quinbay/SpringDemo/service/SearchServiceIMPL.java
+++ src/main/java/com/quinbay/SpringDemo/service/SearchServiceIMPL.java
@@ -6,6 +6,7 @@ import com.quinbay.SpringDemo.dto.ProductResponseDTO;
 import com.quinbay.SpringDemo.dto.ProductRequestSearchDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.quinbay.SpringDemo.constants.solrFieldNames;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,25 +21,23 @@ public class SearchServiceIMPL implements SearchService {
     @Override
     public ProductResponseDTO productSearchResponse(ProductRequestSearchDTO request) {
         Map<String,Object> products = searchClient.getProducts(request.getSearchTerm());
-        // products.get("response").get("docs").
         List<HashMap<String,Object>> list = ((List<HashMap<String,Object>>)((HashMap<String,Object>)products.get("response")).get("docs"));
         List<ProductBean> response = new ArrayList<>();
         for(HashMap<String,Object> mapObj : list){
             ProductBean pb = new ProductBean();
-            if((Integer)mapObj.get("isInStock")>0)
+            if((Integer)mapObj.get(solrFieldNames.IN_STOCK)>0)
                     pb.setInStock(true);
             else{
                 pb.setInStock(false);
             }
-            //System.out.println(pb.isInStock());
-            pb.setSalesPrice(Double.parseDouble(mapObj.get("offerPrice").toString()));
-            //System.out.println(mapObj.get("offerPrice").toString());
-            pb.setTitle(mapObj.get("name").toString());
-            //System.out.println(mapObj.get("name").toString());
-            pb.setDescription(mapObj.get("description").toString());
-            //System.out.println(mapObj.get("description").toString());
+            pb.setSalesPrice(Double.parseDouble(mapObj.get(solrFieldNames.OFFER_PRICE).toString()));
+
+            pb.setTitle(mapObj.get(solrFieldNames.NAME).toString());
+
+            pb.setDescription(mapObj.get(solrFieldNames.DESCRIPTION).toString());
+
             response.add(pb);
-            //System.out.println(response);
+
 
         }
 
